f86bf1013e671fb39f9cd22844835bb1f1b618c3
Dom Corvasce <dom.corvasce@yandex.com>
2016-08-05 13:28:39 +0200

    Fixed a stupid typo

--- lib/hades-sql/Database.php
+++ lib/hades-sql/Database.php
@@ -9,7 +9,7 @@ class Database {
     public $where_condition = null;
 
     public function __construct($driver, $dbname, $user, $password) {
-        $this->pdo = new PDO("$driver:host=localhost;dbname=$dbname", $user, $password);
+        $this->pdo = new \PDO("$driver:host=localhost;dbname=$dbname", $user, $password);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     }
