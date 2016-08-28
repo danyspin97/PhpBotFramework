44adcf34195a14ad0be2602830b27d1175dee095
Dany Spin <danyspin.ds@gmail.com>
2016-08-28 22:51:29 +0000

    Fixed sendMessage

--- lib/hades/CoreBot.php
+++ lib/hades/CoreBot.php
@@ -45,9 +45,9 @@ class CoreBot {
      * $inline_keyboard reply_markup of the message (https://core.telegram.org/bots/api#inlinekeyboardmarkup)
      * $parse_mode Parse mode of the message (https://core.telegram.org/bots/api#formatting-options)
      */
-    public function &sendMessage($text, $chat_id, $inline_keyboard = null, $reply_to = null, $parse_mode = 'HTML', $disable_web_preview = true, $disable_notification = false) {
+    public function &sendMessage($text, $inline_keyboard = null, $reply_to = null, $parse_mode = 'HTML', $disable_web_preview = true, $disable_notification = false) {
         $parameters = [
-            'chat_id' => &$chat_id,
+            'chat_id' => &$this->chat_id,
             'text' => &$text,
             'parse_mode' => $parse_mode,
             'disable_web_page_preview' => $disable_web_preview,
