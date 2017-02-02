a5b475f11cc4a82f65e8abb2c9ee3006dd341614
Dom <dom.corvasce@yandex.com>
2017-02-02 17:41:31 +0100

    Refactor(docs): Clear Localization/Language.php

--- src/Localization/LocalizatedString.php
+++ src/Localization/LocalizatedString.php
@@ -44,25 +44,18 @@ trait LocalizatedString
 
         // If the language of the user is already set in the array containing localizated strings
         if (!isset($this->local[$this->language])) {
-            // Is the bot using webhook?
             if (isset($this->webhook)) {
-                // Load only the user language in array
                 $this->loadSingleLanguage($this->localization_dir);
             } else {
-                // Load all localization files
                 $this->loadLocalization($this->localization_dir);
             }
         }
 
-
-
-        // Check if the string needed exists
+        // Check if the requested string exists
         if (isset($this->local[$this->language][$index])) {
-            // If yes, return it
             return $this->local[$this->language][$index];
         }
 
-        // Throw an error
         throw BotException("$index is not set for {$this->language}");
     }
 
